32083b1a1f9b7b8d298acd2530608a83db7c6930
Victor <victor.p.petrescu@gmail.com>
2020-01-07 14:52:42 -0500

    added loader and modified searchBox  component

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,45 +2,14 @@
 
 import React, { Component } from 'react';
 import SearchBox from './SearchBox.jsx';
-import request from 'superagent'; //Used Superagent since the http library is very simple and userfriendly
 import { connect } from 'react-redux';
 import BookList from './BookList.jsx';
 
 class Books extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchInput: '',
-    };
-  }
-
-  handleSubmit = e => {
-    //prevent form submit
-    e.preventDefault();
-    //superagent makes the request to the api with the query from searchInput
-    request
-      .get('https://www.googleapis.com/books/v1/volumes')
-      .query({ q: this.state.searchInput })
-      .then(data => {
-        this.props.dispatch({ type: 'SEARCHED_BOOKS', books: data.body.items });
-      })
-      .catch(err => {
-        console.log(err.message, err.response);
-      });
-  };
-
-  //modify the state with the user searchInput
-  handleSearch = event => {
-    this.setState({ searchInput: event.target.value });
-  };
-
   render() {
     return (
       <div>
-        <SearchBox
-          handleSubmit={this.handleSubmit}
-          handleSearch={this.handleSearch}
-        />
+        <SearchBox />
         <BookList books={this.props.books} />
       </div>
     );
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,23 +1,68 @@
 //Component for the Search box to search books. handleSubmit and handleSearch have been passed down as props from the Books component.
 
 import React, { Component } from 'react';
+import request from 'superagent'; //Used Superagent since the http library is very simple and userfriendly
+import { connect } from 'react-redux';
 
 class SearchBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: '',
+      loading: false,
+    };
+  }
+
+  handleSubmit = e => {
+    //prevent form submit
+    e.preventDefault();
+    this.setState({ loading: true });
+    //superagent makes the request to the api with the query from searchInput
+    setTimeout(() => {
+      request
+        .get('https://www.googleapis.com/books/v1/volumes')
+        .query({ q: this.state.searchInput })
+        .then(data => {
+          this.props.dispatch({
+            type: 'SEARCHED_BOOKS',
+            books: data.body.items,
+          });
+        })
+        .catch(err => {
+          console.log(err.message, err.response);
+        });
+      this.setState({ loading: false });
+    }, 1500);
+  };
+
+  //modify the state with the user searchInput
+  handleSearch = event => {
+    this.setState({ searchInput: event.target.value });
+  };
+
   render() {
     return (
       <div className="search-box">
-        <form onSubmit={this.props.handleSubmit}>
+        <form onSubmit={this.handleSubmit}>
           <input
-            onChange={this.props.handleSearch}
+            onChange={this.handleSearch}
             placeholder="Search books..."
             type="text"
             className="searchInput"
           />
-          <button type="submit">Search</button>
+          <button
+            onClick={this.handleSubmit}
+            disabled={this.state.loading}
+            type="submit"
+          >
+            {this.state.loading && <i className="fa fa-refresh fa-spin"></i>}
+            {this.state.loading && <span>Getting books...</span>}
+            {!this.state.loading && <span>Search</span>}
+          </button>
         </form>
       </div>
     );
   }
 }
 
-export default SearchBox;
+export default connect(null)(SearchBox);
